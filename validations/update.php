a58d4a7691226ab9fa82ae30e849943b0ea73b03
Michael Douglas <usermich999@gmail.com>
2022-03-30 14:39:10 -0300

    second commit

--- validations/update.php
+++ validations/update.php
@@ -7,7 +7,7 @@ if(count($_POST) > 0)
 { 
     if(isset($_POST["email-edit"]) && verificar_email($_POST["email-edit"]) == 1) 
 { 
-    if(email_cadastrado($_POST["email-edit"]) == 0)
+    if(email_cadastrado($_POST["email-edit"]) == 0 || $_POST["email-edit"] == $_SESSION["email"])
     {
         atualizar($_POST["nome-edit"], $_POST["email-edit"], md5($_POST["senha-edit"]), $_SESSION["id"]);
     }
